a369d40aa3f92e8cb44af74805531df5c2a97693
martinm <marburgermartin143@googlemail.com>
2020-04-30 11:37:12 +0200

    + Verwaltungsseite

--- IvA/Controllers/HomeController.cs
+++ IvA/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace IvA.Controllers
             return View();
         }
 
+        public IActionResult Verwaltung()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
