373596448fccecc1677982b2535c993b5d9a002d
flunschi <floschmitt95@yahoo.de>
2020-05-12 09:21:46 +0200

    Added A Connection String to the Database / Added a ProjectUser Tab / Added Basic Database Interactionfeatures for the ProjectUser Tab

---
+++ IvA/Data/ProjectUserLoginContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using IvA.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// Datenbankkontextklasse für das Datenmodell ProjectUsersLogin. Dient als Schnittstelle zwischen Modell und Datenbank und steuert das Datenbankschema.
+// Stellt Funktionalitäten von EF-Core bereit.
+
+namespace IvA.Data
+{
+    public class ProjectUserLoginContext : DbContext
+    {
+        public ProjectUserLoginContext(DbContextOptions<ProjectUserLoginContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<ProjectUsersLogin> ProjectUserLoginContexts { get; set; }
+
+    }
+}
--- IvA/Startup.cs
+++ IvA/Startup.cs
@@ -28,10 +28,14 @@ namespace IvA
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddDbContext<ProjectUserLoginContext>(options =>
+               options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+
             services.AddControllersWithViews();
             services.AddRazorPages();
         }
