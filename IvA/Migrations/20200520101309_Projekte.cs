ad9ae39c45544ff0cbbf7ac7776e1c1e2c7dc1eb
Flunschi <floschmitt95@yahoo.de>
2020-05-20 12:21:41 +0200

    ProjectUserLogin gelöscht. ProjekteModel/Context/Controller erstellt / Datenbank entsprechend angepasst. Favicon geändert. Json.Developement gelöscht. Scaffolding ReadMe gelöscht. Ivans Html-Code in Projekte index.cshtml eingefügt.

---
+++ IvA/Migrations/20200520101309_Projekte.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IvA.Migrations
+{
+    public partial class Projekte : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Projekte",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Projektname = table.Column<string>(nullable: true),
+                    Projektersteller = table.Column<string>(nullable: true),
+                    Mitglieder = table.Column<string>(nullable: true),
+                    Beschreibung = table.Column<string>(nullable: true),
+                    Deadline = table.Column<DateTime>(nullable: false),
+                    Status = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Projekte", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Projekte");
+        }
+    }
+}
