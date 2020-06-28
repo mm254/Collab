a5412b062f35c043f5f22cefc347b97fc57fdc78
martin <marburgermartin143@googlemail.com>
2020-06-28 14:52:42 +0200

    Dashboard Start

---
+++ IvA/Models/DashboardModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IvA.Models
+{
+    public class DashboardModel
+    {
+        public PaketeUserViewModel UserPackages { get; set; }
+        public ArbeitsPaketModel Packages { get; set; }
+    }
+}
