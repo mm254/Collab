68c96857880ba357cc1093f790b138e1f442b0ad
flunschi <floschmitt95@yahoo.de>
2020-07-07 11:28:47 +0200

    Zwischenstand Zeitmanagement

---
+++ IvA/Data/Migrations/20200707065211_Zeitbedarf.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IvA.Data.Migrations
+{
+    public partial class Zeitbedarf : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "VerbrauchteZeit",
+                table: "ArbeitsPaket",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Zeitbudget",
+                table: "ArbeitsPaket",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "VerbrauchteZeit",
+                table: "ArbeitsPaket");
+
+            migrationBuilder.DropColumn(
+                name: "Zeitbudget",
+                table: "ArbeitsPaket");
+        }
+    }
+}
