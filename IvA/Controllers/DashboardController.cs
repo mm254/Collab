a699b8e7d9db993609d455191b9e2841da424eaa
PhilippRutz <65129322+PhilippRutz@users.noreply.github.com>
2020-07-10 15:43:44 +0200

    Dropdown Dashboard

--- IvA/Controllers/DashboardController.cs
+++ IvA/Controllers/DashboardController.cs
@@ -67,5 +67,28 @@ namespace IvA.Controllers
             sortedDashboard.Sort((x, y) => DateTime.Compare(x.Frist, y.Frist));
             return View(sortedDashboard);
         }
+        public async Task<IActionResult> Done()
+        {
+            var loggedUser = await _userManager.GetUserAsync(this.User);
+            List<PaketeUserViewModel> UsersPackages = _context.PaketeUserViewModel.ToList();
+            var Packages = _context.ArbeitsPaket.ToList();
+            var dashboard = from _packages in Packages
+                            join _userPackages in UsersPackages
+                            on _packages.ArbeitsPaketId equals _userPackages.ArbeitsPaketId
+                            where _userPackages.UserId == loggedUser.Id
+                            select new ArbeitsPaketModel
+                            {
+                                ArbeitsPaketId = _packages.ArbeitsPaketId,
+                                Beschreibung = _packages.Beschreibung,
+                                Frist = _packages.Frist,
+                                Mitglieder = _packages.Mitglieder,
+                                PaketName = _packages.PaketName,
+                                ProjektId = _packages.ProjektId,
+                                Status = _packages.Status
+                            };
+            List<ArbeitsPaketModel> sortedDashboard = dashboard.ToList();
+            sortedDashboard.Sort((x, y) => DateTime.Compare(x.Frist, y.Frist));
+            return View(sortedDashboard);
+        }
     }
 }
